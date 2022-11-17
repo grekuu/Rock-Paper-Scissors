c57442ee7f7e981ceeb38beafcb93d5c446dc349
grekuu <gracjanprusik@gmail.com>
2022-11-17 20:00:00 +0100

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import "./index.scss";
+
 const App = () => {
-  return <div>App</div>;
+  const choices = ["✊", "✋", "✌️"];
+
+  return (
+    <div className="container">
+      <h1 className="title">Rock Paper Scissors</h1>
+      <div className="main-container">
+        <div className="main-container-child">
+          <h3>You: 0</h3>
+        </div>
+        <div className="main-container-child">
+          <h3>Computer: 0</h3>
+        </div>
+      </div>
+      <div className="choices">
+        {choices.map((choice: string) => {
+          return (
+            <div>
+              <button>{choice}</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default App;
